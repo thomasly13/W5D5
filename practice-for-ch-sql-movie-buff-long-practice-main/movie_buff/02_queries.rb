c0a55f958e581e608ad386334ec82f4fe8b7d078
nj <nickgentrymma@gmail.com>
2023-04-28 17:04:03 -0700

    started 02_queries

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -2,10 +2,19 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
   
+  Movie.select(:id, :title, :yr, :score)
+  .where(yr: 1980..1989)
+  .where(score: 3..5)
+
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
+  array = [8, 9, 10]
+  Movie.where.not(score: array)
+  .group(:yr)
+  .pluck(:yr)
+  # .having('COUNT')
   
 end
 
